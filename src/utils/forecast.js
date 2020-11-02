e4c0fdde5895992e8bada8bc04e9339af1fcec3f
Yash Agarwal <yagarwal1307@gmail.com>
2020-11-02 21:17:58 +0530

    Update forecast string with new data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location')
     } else {
-      callback(undefined, body.current.weather_descriptions[0] + '. It is '+ body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+      callback(undefined, body.current.weather_descriptions[0] + '. It is '+ body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The wind speed is ' + body.current.wind_speed)
     }
   })
 }
